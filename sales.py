2420e73f5abb23ae22818096edb37428426b3cae
chaudharyriya2011-ship-it <chaudharyriya2011@gmail.com>
2026-09-22 21:32:23 +0530

    saless

---
+++ sales.py
@@ -0,0 +1,7 @@
+cost = float(input("Enter amount of product "))
+sales = float(input("enter sales amount "))
+if sales>cost :
+    amount = sales-cost
+    print(amount , " Total profit ")
+else :
+    print("no profit")
